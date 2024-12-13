83cfa3a57cd651a85c067e300f02937c275c243b
aubepine <ayoub.lamchichi.info@gmail.com>
2024-12-13 14:58:46 +0100

    Backend
    Endpoints tous testés, erreur 501 Not Implemented sur post et put aussi

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,9 +1,14 @@
 from flask import Flask
+from flask_cors import CORS
 
 def create_app():
     app = Flask(__name__)
 
-    from .routes import main
-    app.register_blueprint(main)
+    # Configuration CORS pour Angular
+    CORS(app)
+
+    # Enregistrer les blueprints
+    from .routes import bp as routes_bp
+    app.register_blueprint(routes_bp)
 
     return app
